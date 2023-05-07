5c611c3e6f384d49847546d238dae455d64b447e
Shohei Kameda <shoheik@cpan.org>
2023-05-08 06:21:23 +0900

    first

---
+++ Gemfile
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in llm_memory_gmail_loader.gemspec
+gemspec
+
+gem "rake", "~> 13.0"
+
+gem "rspec", "~> 3.0"
+
+gem "standard", "~> 1.3"
---
+++ spec/llm_memory_gmail_loader_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe LlmMemoryGmailLoader do
+  it "has a version number" do
+    expect(LlmMemoryGmailLoader::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
