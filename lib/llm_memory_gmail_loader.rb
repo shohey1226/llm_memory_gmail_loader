cdf61510a20b312fc91c35127a2f0a8cfcf0ecdb
Shohei Kameda <shoheik@cpan.org>
2023-05-09 10:16:10 +0900

    updated to have create_documents

--- lib/llm_memory_gmail_loader.rb
+++ lib/llm_memory_gmail_loader.rb
@@ -50,18 +50,21 @@ module LlmMemoryGmailLoader
       # authorizer.sub = "user@example.com" # Replace with the user's email address
     end
 
-    def list_emails(email)
+    def list_emails(email:, limit: 100)
       @service.authorization.sub = email
       query = "label:sent" # Fetch all sent emails
       next_page_token = nil
       sent_emails = []
-
+      count = 0
+      max_results = (limit < 100) ? limit : 100
       loop do
-        result = @service.list_user_messages("me", q: query, page_token: next_page_token)
+        result = @service.list_user_messages("me", q: query, page_token: next_page_token, max_results: max_results)
         sent_emails.concat(result.messages) if result.messages
+        count += result.messages.length
 
-        # next_page_token = result.next_page_token
+        next_page_token = result.next_page_token
         break if next_page_token.nil?
+        break if count > limit
       end
 
       sent_emails
@@ -70,6 +73,15 @@ module LlmMemoryGmailLoader
     def get_email_subject(headers)
       subject_header = headers.find { |header| header.name == "Subject" }
       subject_header&.value
+    end
+
+    def get_email_from(headers)
+      from_header = headers.find { |header| header.name == "From" }
+      if from_header
+        from_header.value
+      else
+        ""
+      end
     end
 
     def extract_email_bodies(payload)
@@ -102,43 +114,63 @@ module LlmMemoryGmailLoader
       end
     end
 
-    def print_email_details(emails)
+    def extract_text_from_html(html)
+      utf8_html = html.force_encoding("UTF-8")
+      document = Nokogiri::HTML(utf8_html)
+      document.css("style").remove
+      document.css("br").each { |br| br.replace("\n") }
+      document.css("a").each do |a|
+        link = a["href"]
+        text = a.text
+        a.replace("#{text} (#{link})") # Replace the <a> tag with its text content and link
+      end
+      document.text.gsub(/　/, " ").gsub(/\s+/, " ").gsub(/(\r\n)+|\n+/, "\n").strip
+    end
+
+    def create_documents(emails)
+      docs = []
       emails.each do |email|
         message = @service.get_user_message("me", email.id)
         subject = get_email_subject(message.payload.headers)
+        from = get_email_from(message.payload.headers)
         bodies = extract_email_bodies(message.payload)
+        if bodies[:text]
+          body = bodies[:text].force_encoding("UTF-8").gsub(/　/, " ").gsub(/\s+/, " ").strip
+        elsif bodies[:html]
+          body = extract_text_from_html(bodies[:html])
+        else
+          next
+        end
 
-        puts "Email ID: #{email.id}, Subject: #{subject}"
-        puts "Plain text body: #{bodies[:text]}" if bodies[:text]
-        puts "HTML body: #{extract_text_from_html(bodies[:html])}" if bodies[:html]    
-        puts "------------------------------------------------------"
+        docs.push({
+          content: "#{subject}\n#{body}",
+          metadata: {
+            subject: subject,
+            from: from
+          }
+        })
       end
-    end
-
-    def extract_text_from_html(html)
-      document = Nokogiri::HTML(html)
-      document.css('style').remove
-      document.css('br').each { |br| br.replace("\n") }
-      document.css('a').each do |a|
-        link = a['href']
-        text = a.text
-        a.replace("#{text} (#{link})") # Replace the <a> tag with its text content and link
-      end      
-      document.text.gsub(/　/," ").gsub(/\s+/, " ").strip
+      docs
     end
 
     def load(args)
       emails = args[:emails]
+      limit = args[:limit]
+      raise "emails is required" if emails.nil? || emails.length == 0
 
       @service ||= Google::Apis::GmailV1::GmailService.new
       @service.authorization = authorize
 
       results = []
       emails.each do |email|
-        results += list_emails(email)
+        results += if limit.nil?
+          list_emails(email: email)
+        else
+          list_emails(email: email, limit: limit)
+        end
       end
 
-      print_email_details(results)
+      create_documents(results)
     end
   end
 end
